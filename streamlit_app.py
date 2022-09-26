825add7a5571ad90b33e4d3f0e07b8373e06f6a2
Mani <114245506+Mani6788@users.noreply.github.com>
2022-09-26 13:13:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,5 @@ import pandas
 
 my_fruits_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruits_list)
-streamlit.title("My Parents new diner") 
-streamlit.header("breakfast menu")
-streamlit.text("this is a text message")
+streamlit.multiselect ("pick some fruits:", list(my_fruit_list.index))
+stramlit.dataframe(my_fruit_list)
