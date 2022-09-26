d0bc9eec2e2cce52dee2c14b7fc1d6ebedf79307
Mani <114245506+Mani6788@users.noreply.github.com>
2022-09-26 13:10:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 
 import streamlit
+
+my_fruits_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
 streamlit.title("My Parents new diner") 
 streamlit.header("breakfast menu")
 streamlit.text("this is a text message")
